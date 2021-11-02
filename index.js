0151f8d3e66d6f1e9a525d1fe283691237238acf
cnrd <hello@conradweise.com>
2021-11-02 22:45:21 +0100

    init

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 const { ImapFlow } = require('imapflow')
+const nodemailer = require('nodemailer')
+const { Telegraf } = require('telegraf')
 
 const client = new ImapFlow({
   host: 'imap.gmail.com',
@@ -11,7 +13,36 @@ const client = new ImapFlow({
   }
 })
 
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.MAIL_USERNAME,
+    pass: process.env.MAIL_PASSWORD
+  }
+})
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+const state = {
+  subscribers: []
+}
+
 console.log(process.env.MAIL_USERNAME, process.env.MAIL_PASSWORD)
+
+async function send (address, subject, text) {
+  transporter.sendMail({
+    from: process.env.MAIL_USERNAME,
+    to: address,
+    subject,
+    text
+  }, (error, info) => {
+    if (error) {
+      console.log(error)
+    } else {
+      console.log('email sent', address, subject)
+    }
+  })
+}
 
 async function check () {
   await client.connect()
@@ -22,8 +53,15 @@ async function check () {
     console.log(message.source.toString())
 
     for await (let message of client.fetch('1:*', { envelope: true })) {
-      if (message.subject === 'add me') {
-        console.log(message)
+      if (message.envelope.subject.includes('add me') && message.envelope.subject.split(',')[1]) {
+        const address = message.envelope.from[0].address
+        const chatId = Number(message.envelope.subject.split(',')[1].trim())
+
+        state.subscribers.push({
+          address,
+          chatId
+        })
+        send(address, `u r in w/ ${chatId}`, '')
       }
     }
   } finally {
@@ -31,6 +69,15 @@ async function check () {
   }
 
   await client.logout()
+  return
 }
 
-check()/* .catch(err => console.error(err)) */
+async function init () {
+  await check()/* .catch(err => console.error(err)) */
+  setInterval(() => {
+    console.log(state)
+  }, 1000 * 60)
+}
+
+init()
+
